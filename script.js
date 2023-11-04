088e45f311d7ba8b5376d802f185c9ec42ce3c50
Atom <nykorak19@gmail.com>
2023-11-04 15:11:44 +0200

    les13

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const arrayLength = parseInt(prompt("Введіть кількість масивів:"));
+const myArray = [];
+
+for (let i = 0; i < arrayLength; i++) {
+    const element = parseInt(prompt(`Введіть ${i + 1}-й елемент:`));
+    myArray.push(element);
+}
+console.log("Початковий масив:", myArray);
+
+myArray.sort((a, b) => a - b);
+console.log("Відсортований масив:", myArray);
+
+myArray.splice(1, 4);
+console.log("Масив після видалення елементів від 2 по 4:", myArray);
+
+
